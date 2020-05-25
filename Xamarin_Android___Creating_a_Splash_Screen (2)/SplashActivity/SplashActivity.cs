e87c7dad669916cde9db93cd60392e2fa1adbe53
XfHua <jasonHuaxf@163.com>
2020-05-25 11:17:41 +0800

    Add files via upload

---
+++ Xamarin_Android___Creating_a_Splash_Screen (2)/SplashActivity/SplashActivity.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Widget;
+
+namespace com.xamarin.sample.splashscreen
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher =true,NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof (SplashActivity).Name;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Splash);
+
+            FindViewById<TextView>(Resource.Id.txtAppVersion).Text = $"Version {PackageManager.GetPackageInfo(PackageName, 0).VersionName}";
+
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            await Task.Delay(5000); // Simulate a bit of startup work.
+            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
